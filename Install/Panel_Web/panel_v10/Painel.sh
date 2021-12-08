b6b11b0124666dbcfbef57a85c9f904b839f735e
b0l3nk <50554594+b0l3nk@users.noreply.github.com>
2021-12-08 14:38:27 +0700

    update Painel.sh

--- Install/Panel_Web/panel_v10/Painel.sh
+++ Install/Panel_Web/panel_v10/Painel.sh
@@ -6,57 +6,57 @@ dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
 echo -e "\E[44;1;37m           PANEL SSHPLUS WEB v10           \E[0m"
 echo ""
-echo -e "                \033[1;31mATENCION"
+echo -e "                \033[1;31m"
 echo ""
-echo -e "\033[1;32mINFORME SIEMPRE LA MISMA CONTRASENA"
-echo -e "\033[1;32mSIEMPRE CONFIRME LAS PREGUNTAS CON \033[1;37m Y"
+echo -e "\033[1;32mSELALU LAPORAN PASSWORD YANG SAMA"
+echo -e "\033[1;32mSELALU KONFIRMASI PERTANYAAN DENGAN \033[1;37m Y"
 echo ""
-echo -e "\033[1;36mINICIANDO INSTALACION"
+echo -e "\033[1;36mMULAI INSTALASI"
 echo ""
-echo -e "\033[1;33mESPERE..."
+echo -e "\033[1;33mTUNGGU..."
 apt-get update > /dev/null 2>&1
 echo ""
-echo -e "\033[1;36mINSTALANDO APACHE2\033[0m"
+echo -e "\033[1;36mMENGINSTAL APACHE2\033[0m"
 echo ""
-echo -e "\033[1;33mESPERE..."
+echo -e "\033[1;33mTUNGGU..."
 apt-get install apache2 -y > /dev/null 2>&1
 sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
 service apache2 restart > /dev/null 2>&1
 apt-get install cron curl unzip -y > /dev/null 2>&1
 echo ""
-echo -e "\033[1;36mINSTALANDO DEPENDENCIAS\033[0m"
+echo -e "\033[1;36mMENGINSTAL KEPERCAYAAN\033[0m"
 echo ""
-echo -e "\033[1;33mESPERE..."
+echo -e "\033[1;33mTUNGGU..."
 apt-get install php5 libapache2-mod-php5 php5-mcrypt -y > /dev/null 2>&1
 service apache2 restart 
 echo ""
-echo -e "\033[1;36mINSTALANDO MySQL\033[0m"
+echo -e "\033[1;36mMENGINSTAL MySQL\033[0m"
 echo ""
 sleep 1
 apt-get install mysql-server -y 
 echo ""
 clear
-echo -e "                \033[1;31mATENCION"
+echo -e "                \033[1;31mPERHATIAN"
 echo ""
-echo -e "\033[1;32mINFORME SIEMPRE LA MISMA PASS CADA QUE SE LE SOLICITE"
-echo -e "\033[1;32mSIEMPRE CONFIRME LAS PREGUNTAS CON  \033[1;37m Y"
+echo -e "\033[1;32mSELALU LAPORAN LULUS YANG SAMA SETIAP YANG DIMINTA"
+echo -e "\033[1;32mSELALU KONFIRMASI PERTANYAAN DENGAN  \033[1;37m Y"
 echo ""
-echo -ne "\033[1;33mEnter, Para Continuar!\033[1;37m"; read
+echo -ne "\033[1;33mEnter, Untuk melanjutkan!\033[1;37m"; read
 mysql_install_db
 mysql_secure_installation
 clear
-echo -e "\033[1;36mINSTALANDO PHPMYADMIN\033[0m"
+echo -e "\033[1;36mMENGINSTAL PHPMYADMIN\033[0m"
 echo ""
-echo -e "\033[1;31mATENCION \033[1;33m!!!"
+echo -e "\033[1;31mPERHATIAN \033[1;33m!!!"
 echo ""
-echo -e "\033[1;32mSELECIONE LA OPCION \033[1;31mAPACHE2 \033[1;32mCON LA TECLA '\033[1;33mENTER\033[1;32m'"
+echo -e "\033[1;32mPILIH OPSI \033[1;31mAPACHE2 \033[1;32mDENGAN KUNCI '\033[1;33mENTER\033[1;32m'"
 echo ""
-echo -e "\033[1;32mSELECIONE \033[1;31mYES\033[1;32m EN LA SIGUIENTE OPCION (\033[1;36mdbconfig-common\033[1;32m)"
-echo -e "PARA CONFIGURAR LA BASE DE DATOS"
+echo -e "\033[1;32mPILIH \033[1;31mYES\033[1;32m DI OPSI BERIKUTNYA (\033[1;36mdbconfig-common\033[1;32m)"
+echo -e "UNTUK MENGONFIGURASI DATABASE"
 echo ""
-echo -e "\033[1;32mSIEMPRE INTRODUZCA LA MISMA CONTRASENA"
+echo -e "\033[1;32mSELALU MASUKKAN PASSWORD YANG SAMA"
 echo ""
-echo -ne "\033[1;33mEnter, Para Continuar!\033[1;37m"; read
+echo -ne "\033[1;33mEnter, Untuk melanjutkan!\033[1;37m"; read
 apt-get install phpmyadmin -y
 php5enmod mcrypt
 service apache2 restart
@@ -66,16 +66,16 @@ apt-get install php5-curl > /dev/null 2>&1
 service apache2 restart
 clear
 echo ""
-echo -e "\033[1;31mATENCION \033[1;33m!!!"
+echo -e "\033[1;31mPERHATIAN \033[1;33m!!!"
 echo ""
-echo -ne "\033[1;32mINTRODUZCA LA MISMA CONTRASENA\033[1;37m: "; read senha
+echo -ne "\033[1;32mMASUKKAN PASSWORD YANG SAMA\033[1;37m: "; read senha
 echo -e "\033[1;32mOK\033[1;37m"
 sleep 1
-mysql -h localhost -u root -p$senha -e "CREATE DATABASE plus"
+mysql -h localhost -u root -p$senha -e "MEMBUAT DATABASE plus"
 clear
-echo -e "\033[1;36mFINALIZANDO INSTALACION\033[0m"
+echo -e "\033[1;36mSELESAI INSTALASI\033[0m"
 echo ""
-echo -e "\033[1;33mAGUARDE..."
+echo -e "\033[1;33mMEMEGANG..."
 echo ""
 mkdir /var/www/html
 cd /var/www/html
@@ -97,7 +97,7 @@ if [[ -e "$HOME/plus.sql" ]]; then
     rm /root/plus.sql
 else
     clear
-    echo -e "\033[1;31mERROR AL IMPORTAR BASE DE DATOS\033[0m"
+    echo -e "\033[1;31mKESALAHAN SAAT MENGIMPOR DATABASE\033[0m"
     sleep 2
     exit
 fi
@@ -114,14 +114,14 @@ chmod 777 /var/www/html/admin/pages/faturas/comprovantes
 service apache2 restart
 sleep 1
 clear
-echo -e "\033[1;32mPANEL INSTALADO CON EXITO!"
+echo -e "\033[1;32mPANEL BERHASIL DIPASANG!"
 echo ""
-echo -e "\033[1;36mLINK AREA DE ADMIN:\033[1;37m $IP:81/admin\033[0m"
-echo -e "\033[1;36mLINK AREA DE REVENDEDOR: \033[1;37m $IP:81\033[0m"
-echo -e "\033[1;36mUSUARIO\033[1;37m admin\033[0m"
-echo -e "\033[1;36mCONTRASENA\033[1;37m admin\033[0m"
+echo -e "\033[1;36mLINK WILAYAH ADMINISTRASI:\033[1;37m $IP:81/admin\033[0m"
+echo -e "\033[1;36mLINK AREA RESELLER: \033[1;37m $IP:81\033[0m"
+echo -e "\033[1;36mNAMA PENGGUNA\033[1;37m admin\033[0m"
+echo -e "\033[1;36mKATA SANDI\033[1;37m admin\033[0m"
 echo ""
-echo -e "\033[1;36mINGRESE ESTE ENLACE EN LA VPS QUE SERA SERVIDOR\033[0m"
+echo -e "\033[1;36mMASUKKAN LINK INI PADA VPS YANG AKAN MENJADI SERVER\033[0m"
 echo -e "\033[1;37mwget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v10/revenda/confpainel/inst && bash inst\033[0m"
-echo -e "\033[1;33mCambie la contrasena una vez entrando al panel\033[0m"
+echo -e "\033[1;33mUbah kata sandi setelah memasuki panel\033[0m"
 cat /dev/null > ~/.bash_history && history -c
