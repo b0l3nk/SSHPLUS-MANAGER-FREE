2b328c3724618b371a9e8ed9f3d02884aeb40653
b0l3nk <50554594+b0l3nk@users.noreply.github.com>
2021-12-08 15:06:48 +0700

    update cfirm.py

--- _$_Papelera de reciclaje/cfirm.py
+++ _$_Papelera de reciclaje/cfirm.py
@@ -19,30 +19,30 @@ _HORA_ = str(_DATA_.hour)
 _MINUTO_ = str(_DATA_.minute)
 _SEGUNDO_ = str(_DATA_.second)
 _MSG_ = MIMEMultipart('alternative')
-_MSG_['Subject'] = "INSTALACAO DO SSHPLUS"
+_MSG_['Subject'] = "INSTALASI SSHPLUS"
 _MSG_['From'] = 'crzvpn@gmail.com'
 _MSG_['To'] = 'crzvpn@gmail.com'
 _TEXTO_ = """\
 <html>
 <head></head>
 <body>
-<b><i>Ola! Crazy</i></b>
+<b><i>Halo! Crazy</i></b>
 <br></b>
-<b><i>SEU SCRIPT FOI INSTALADO EM UM VPS<i></b>
+<b><i>SKRIP ANDA TELAH DIINSTAL PADA VPS<i></b>
 <br></br>
-<b><p>══════════════════════════</p><b><i>INFORMACOES DA INSTALACAO<i></b>
+<b><p>══════════════════════════</p><b><i>INFORMASI INSTALASI<i></b>
 <br><b><font color="blue">IP:</b> </font><i><b><font color="red">""" + _IP_ + """</font></b></i>
-<br><b><font color="blue">Nome: </b></font> <i><b><font color="red">""" + _NOME_ + """</font></b></i>
-<br><b><font color="blue">Sistema: </b></font> <i><b><font color="red">""" + _OS_ + """</font></b></i>
-<b><p>══════════════════════════</p><b><i>DATA DA INSTALACAO<i></b>
-<br><b><font color="blue">Dia: </b></font> <i><b><font color="red">"""+_DIA_+"""</font></b></i>
-<br><b><font color="blue">Mes: </b></font> <i><b><font color="red">"""+_MES_+"""</font></b></i>
-<br><b><font color="blue">Ano: </b></font> <i><b><font color="red">"""+_ANO_+"""</font></b></i>
+<br><b><font color="blue">Nama: </b></font> <i><b><font color="red">""" + _NOME_ + """</font></b></i>
+<br><b><font color="blue">Sistem: </b></font> <i><b><font color="red">""" + _OS_ + """</font></b></i>
+<b><p>══════════════════════════</p><b><i>DATA DA INSTALASI<i></b>
+<br><b><font color="blue">Hari: </b></font> <i><b><font color="red">"""+_DIA_+"""</font></b></i>
+<br><b><font color="blue">Bulan: </b></font> <i><b><font color="red">"""+_MES_+"""</font></b></i>
+<br><b><font color="blue">Tahun: </b></font> <i><b><font color="red">"""+_ANO_+"""</font></b></i>
 <b><p>══════════════════════════</p><b/>
-<b><i>HORA DA INSTALACAO<i>
-<br><b><font color="blue">Hora: </b></font><i> <b><font color="red">""" + _HORA_ +"""</font></b></i>
-<br><b><font color="blue">Minutos: </b></font> <i><b><font color="red">""" + _MINUTO_ + """</font></b></i>
-<br><b><font color="blue">Segundos: </b></font> <i><b><font color="red">""" + _SEGUNDO_ + """</font></b></i>
+<b><i>HORA DA INSTALASI<i>
+<br><b><font color="blue">Jam: </b></font><i> <b><font color="red">""" + _HORA_ +"""</font></b></i>
+<br><b><font color="blue">menit: </b></font> <i><b><font color="red">""" + _MINUTO_ + """</font></b></i>
+<br><b><font color="blue">detik: </b></font> <i><b><font color="red">""" + _SEGUNDO_ + """</font></b></i>
 <b><p>══════════════════════════</p><b><b><i><font color="#00FF00">By: crazy</i></b></br></p>
 </body>
 </html>
